245518baf8617f0afafb7f97549e5d6993fb9310
bhavnapatil96 <patilbhavna96@gmail.com>
2018-02-16 17:09:25 +0530

    File Added

--- routes.js
+++ routes.js
@@ -5,9 +5,10 @@ module.exports=(app,passport)=>{
 
     app.get('/',(req,res)=>{
         console.log("In get ");
-        res.send({msg:"success"});
+        res.send({msg:"error"});
     });
-    app.get('/profile',(req,res)=>{
+    app.get('/profile1',(req,res)=>{
+        console.log('profile')
         res.send({msg:"user"});
     });
 
@@ -19,10 +20,11 @@ module.exports=(app,passport)=>{
     );
 
     app.get('/auth/google/callback',passport.authenticate('google', {
-
-            successRedirect: '/profile',
+            successRedirect:'http://localhost:3001/list',
             failureRedirect: '/'
         }
-    ));
+    ),(req,res)=>{
+        res.send("By Hardik Patel")
+    });
 
 };
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var bodyParser=require('body-parser');
 var _=require('lodash');
 var conn=require('./db/conn');
 var session = require('express-session');
-
+const {students}=require('./models/students');
 const Student=require('./models/students').students;
 const bcrypt=require('bcryptjs');
 const passport=require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var configAuth=require("./config/auth");
+var token;
 var app=express();
 app.use(passport.initialize());
 app.use(passport.session());
@@ -19,34 +21,45 @@ app.use(session({secret: 'ssshhhhh'}));
 
 app.use((req,res,next) =>{
 
-    res.header('Access-Control-Allow-Origin',' http://localhost:3001');
+    /*res.header('Access-Control-Allow-Origin','*');*/
+    console.log('request  : ',req.headers.origin);
+   // res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header('Access-Control-Allow-Origin','http://localhost:3000');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(`Access-Control-Allow-Methods`, `POST`);
+    res.header(`Access-Control-Expose-Headers`, `x-auth`);
     next();
 });
+
+app.get('/sucess',(req,res)=>{
+   console.log('After Login')
+});
 app.post('/student/add',(req,res)=>{
-    // if (!req.files)
+    console.log('File :',req.body.photo);
+    // if (!req.body.files)
     //     return res.status(400).send('No files were uploaded.');
     //
     // // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    // let sampleFile = req.files.file;
-    //
-    // // Use the mv() method to place the file somewhere on your server
-    // sampleFile.mv('./tmp/'+sampleFile.name, function(err) {
-    //     if (err)
-    //         return res.status(500).send(err);
-    //
-    //     res.send('File uploaded!');
-    // });
+    // let sampleFile = req.body.files.file;
+    // // console.log('File ',sampleFile);
+    // // // Use the mv() method to place the file somewhere on your server
+    // // sampleFile.mv('./tmp/'+sampleFile.name, function(err) {
+    // //     if (err)
+    // //         return res.status(500).send(err);
+    // //
+    // //     res.send('File uploaded!');
+    // // });
 
-  var student=new Student({
+    var student=new Student({
         fullname:req.body.fullname,
         email:req.body.email,
         password:req.body.password,
         contact:req.body.contact,
         gender:req.body.gender,
         city:req.body.city,
-        iagree:req.body.iagree
+        iagree:req.body.iagree,
+        photo:req.body.photo
     });
 
     student.save().then((data)=>{
@@ -62,13 +75,14 @@ app.post('/student/add',(req,res)=>{
     });
 
 });
-
 app.get('/student/list',(req,res)=>{
     Student.find().then((data)=>{
         if(!data){
             console.log(`Data Not found`);
         }
-        res.send(data);
+        console.log(data)
+        res.header('x-auth',token).send(data);
+
     }).catch((e)=>{
         console.log(`Error : ${e.message}`);
     })
@@ -119,7 +133,20 @@ app.post('/student/update',(req,res)=>{
         console.log(`Error : ${e.message}`);
     });
 });
-
+app.post('/student/update',(req,res)=>{
+    let body=_.pick(req.body,['id','fullname','email','contact','gender','city']);
+    //let id=body.id;
+    console.log('request Id : ',req.body.id);
+    Student.findByIdAndUpdate(req.body.id,{$set:body}).then((student)=>{
+        if(!student){
+            console.log(req.body.id,`Id Not Found`);
+            res.status(404).send();
+        }
+        res.send(student);
+    }).catch((e)=>{
+        console.log(`Error : ${e.message}`);
+    });
+});
 app.post('/student/login',(req,res)=>{
     let email=req.body.email;
     let password=req.body.password;
@@ -188,7 +215,11 @@ passport.use(new LocalStrategy ((username, password, done) => {
                     }
                     else
                     {
+
                         console.log("valid password.....");
+                        console.log( "User   : ",user);
+                        token=user.tokens[0].token;
+                        console.log( "token   : ",token);
                     }
                     return done(null, user)
                 });
@@ -206,9 +237,62 @@ app.post('/student/loginp',passport.authenticate('local', {
 
 }));
 //............................................................
+//Gmail Login.........................................................
+app.use((req,res,next)=>{
+    next();
+});
 
+passport.use('google',new GoogleStrategy({
+        clientID: configAuth.googleAuth.clientID,
+        clientSecret: configAuth.googleAuth.clientSecret,
+        callbackURL: configAuth.googleAuth.callbackURL
+    },
+    // google will send back the token and profile
+    (accessToken, refreshToken, profile, done)=> {
+        // asynchronous // Event Loop
+        console.log("In fb use");
+
+        process.nextTick(()=> {
+
+            students.findOne({'google.id' : profile.id }, (err, user)=> {
+
+                console.log('Profile Id :',profile.id)
+
+                // if there is an error, stop everything and return that error connecting to the database
+                if (err) return done(err);
+
+                // if the user is found, then log them in
+                if (user) {
+                    return done(null, user); // user found, return that user
+                } else {
+                    // if there is no user found with that facebook id, create them
+                    var newUser = new students({
+                        id    : profile.id, // set the users gg id
+                        token : accessToken, // we will save the token that gg provides to the user
+                        fullname  :profile.displayName, // look at the passport user profile to see how names are returned
+                        email :profile.emails[0].value // gg can return multiple emails so we'll take the first
+                    });
+
+                    // set all of the gg information in our gg model
+
+
+                    // save our user to the database
+                    newUser.save().then((doc)=>{
+
+                        console.log("Saved User :: = "+doc);
+                         return done(null, doc)
+                    }).catch((err)=>{
+                        console.log("User Error :: = "+err);
+                        return done(err);
+                    });
+                }
+
+            });
+        });
+
+    }));
 //............................................................
-
+require('./routes')(app,passport);
 app.listen(8081,()=>{
     console.log('App is running on Port  8081');
 });
